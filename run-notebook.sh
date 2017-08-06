d2abdb7603ece1e16aeca514a24a311e0b8dfbbd
Derek Larson <larson.derek.a@gmail.com>
2017-08-06 14:33:22 -0700

    Random checkpoint

--- run-notebook.sh
+++ run-notebook.sh
@@ -1,5 +1,4 @@
 #head to http://localhost:8888 after running this (must have Docker installed locally btw)
 #only a good idea to run this locally...
-#Details on the notebook used can be found at: https://github.com/slarson/docker-stacks/blob/master/scipy-notebook/README.md
-#docker run -d -p 8888:8888 --user root -e GRANT_SUDO=yes -v `pwd`:/home/jovyan/work --name scipy-notebook slarson/scipy-notebook
+#Details on the notebook used can be found at: docker-stats at Github
 docker run -d -p 8888:8888 --user root -e GRANT_SUDO=yes -v `pwd`:/home/jovyan/work --name scipy-notebook test_jup
