54913cfbcc2f34e23b3a6e747a5af05a890f88b3
Jeff L Jones <jeffljones@gmail.com>
2017-08-07 20:04:15 -0700

    don't detach, and use scipy-notebook instead of test_jup

--- run-notebook.sh
+++ run-notebook.sh
@@ -1,4 +1,4 @@
 #head to http://localhost:8888 after running this (must have Docker installed locally btw)
 #only a good idea to run this locally...
 #Details on the notebook used can be found at: docker-stats at Github
-docker run -d -p 8888:8888 --user root -e GRANT_SUDO=yes -v `pwd`:/home/jovyan/work --name scipy-notebook test_jup
+docker run -p 8888:8888  --user root -e GRANT_SUDO=yes -v `pwd`:/home/jovyan --name scipy-notebook jupyter/scipy-notebook
